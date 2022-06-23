f771f8cab6ecd6bbb01a403abba06e6c06c7eb17
Vladimir Sumarov <summeroff@gmail.com>
2022-06-23 13:22:45 -0700

    fix scripts

--- ci/build.bat
+++ ci/build.bat
@@ -6,7 +6,7 @@ cd build
 cmake .. ^
     -G"%CMakeGenerator%" -A x64 ^
     -DProtobuf_DIR=%CD%\build\dist\protobuf_dist ^
-    -DgRPC_DIR==%CD%\build\dist\grpc_dist
+    -DgRPC_DIR==%CD%\build\dist\grpc_dist ^
     -DCMAKE_INSTALL_PREFIX="%INSTALL_PACKAGE_PATH%"
 
 cmake --build . --target install --config %BUILD_CONFIGURATION%
