0e001e168d7cdd791b2b7f0adcba2c8117b3fbd4
Vladimir Sumarov <summeroff@gmail.com>
2022-06-23 14:18:58 -0700

    fix scripts

--- ci/build.bat
+++ ci/build.bat
@@ -5,8 +5,8 @@ cd build
 
 cmake .. ^
     -G"%CMakeGenerator%" -A x64 ^
-    -DProtobuf_DIR=%CD%\build\dist\protobuf_dist ^
-    -DgRPC_DIR==%CD%\build\dist\grpc_dist ^
+    -DProtobuf_DIR=%CD%\dist\protobuf_dist ^
+    -DgRPC_DIR==%CD%\dist\grpc_dist ^
     -DCMAKE_INSTALL_PREFIX="%INSTALL_PACKAGE_PATH%"
 
-cmake --build . --target install --config %BUILD_CONFIGURATION%
+cmake --build . --target install --parallel 2 --config %BUILD_CONFIGURATION%
--- ci/build_grpc.bat
+++ ci/build_grpc.bat
@@ -1,7 +1,8 @@
 set GRPC_VERSION=v1.47.0
 set CMakeGenerator=Visual Studio 17 2022
+set GRPC_DIST=%CD%\build\dist\grpc_dist\
 
-if exist build/dist/grpc_dist/ (
+if exist "%GRPC_DIST%" (
     exit 0
 )
 
@@ -14,5 +15,5 @@ cd grpc
 md .build
 cd .build
  
-cmake ..  -G"%CMakeGenerator%"  -A x64  -DgRPC_INSTALL=ON -DCMAKE_INSTALL_PREFIX="..\..\grpc_dist" -DgRPC_BUILD_TESTS=OFF 
-cmake --build . --target install --config Release
+cmake ..  -G"%CMakeGenerator%"  -A x64  -DgRPC_INSTALL=ON -DCMAKE_INSTALL_PREFIX="%GRPC_DIST%" -DgRPC_BUILD_TESTS=OFF 
+cmake --build . --target install --parallel 2 --config Release
