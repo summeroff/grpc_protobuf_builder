808c629310bc0fbc2d121b2fa39393b5c55c6512
Vladimir Sumarov <summeroff@gmail.com>
2022-06-23 23:18:31 -0700

    update script

--- ci/win-build.cmd
+++ ci/win-build.cmd
@@ -2,7 +2,7 @@ echo on
 set CMakeGenerator=Visual Studio 16 2019
 set MAIN_DIR=%CD%
 
-call ci\win-install-protobuf.cmd
+echo call ci\win-install-protobuf.cmd
 call ci\win-install-grpc.cmd
 
 echo %CD%
@@ -13,7 +13,7 @@ cd build
 
 cmake .. ^
     -G"%CMakeGenerator%" -A x64 ^
-    -DProtobuf_DIR="%PROTOBUF_DIST%\cmake" ^
+    -DProtobuf_DIR="%GRPC_DIST%\cmake" ^
     -DgRPC_DIR="%GRPC_DIST%\lib\cmake\grpc" ^
     -Dabsl_DIR="%GRPC_DIST%\lib\cmake\absl" ^
     -DCMAKE_INSTALL_PREFIX="%INSTALL_PACKAGE_PATH%"
